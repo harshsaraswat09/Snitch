f47aa6a1cc43ba5f217beea83bca452932cf8385
Harsh Saraswat <harshsaraswat09@gmail.com>
2026-04-16 06:33:15 +0530

    implement product creation workflow with ImageKit storage, validation, and seller authentication

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -19,6 +19,10 @@ if(!process.env.GOOGLE_CLIENT_SECRET) {
     throw new Error("GOOGLE_CLIENT_SECRET must be defined")
 }
 
+if (!process.env.IMAGEKIT_PRIVATE_KEY){
+    throw new Error("IMAGEKIT_PRIVATE_KEY must be defined")
+
+}
 
 export const config = {
     MONGO_URI: process.env.MONGO_URI,
@@ -26,4 +30,6 @@ export const config = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NODE_ENV: process.env.NODE_ENV || "development",
+    IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY
+
 }
